052cadc5c39640477d5d80bc9dcd4111398f9e3d
Mahfuza <mahfuzasharmili@gmail.com>
2022-02-01 19:48:30 +0600

    2nd lab

---
+++ Products/Products/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Products.Models;
+
+namespace Products.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Productlist> products = new List<Productlist>();
+            for(int i=0; i<10; i++)
+            {
+                var p = new Productlist
+                {
+                    ID = i + 1,
+                    Name = "Product" + (i + 1),
+                    
+                };
+                products.Add(p);
+            }
+            return View(products);
+        }
+    }
+}
---
+++ Products/Products/Models/Productlist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Products.Models
+{
+    public class Productlist
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+    }
+}
